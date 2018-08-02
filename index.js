c08c0bcbe9f15a77b4fb39feab1e85e398c1da87
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 18:15:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function justInvoke(callback){
     return callback.call(window)
 }
-function setThisWithCall(callback){
-  
+function setThisWithCall(callback, arg){
+  return callback.call(window, arg)
 }
