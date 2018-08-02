f3bf9fac3a1500a4f2ee4c50f31360d5d47b84df
Naomi Dennis <njdennis94@gmail.com>
2018-08-02 18:26:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ function setThisWithApply(callback, scope, args){
   return callback.apply(scope, args)
 }
 function returnNewFunctionOf(callback){
-  return ((..args) => { callback["fred"]["name"] = value }).bind(this)
+  return ((..args) => { callback["fred"]["name"] = args[0] }).bind(this)
 }
